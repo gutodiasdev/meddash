013a0707ab7779412ff247d4bdc830ec36afd462
Augusto Dias <gutodiasux@gmail.com>
2023-02-08 23:34:52 -0300

    feat(components): finish desktop extra large Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,73 @@
-import { Box } from '@chakra-ui/react'
+import { Box, Button, Flex, Grid } from '@chakra-ui/react'
+import { Poppins } from '@next/font/google'
 import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
 
-type HeaderProps = {
-  title?: string
-}
+const poppins = Poppins({ weight: '600', subsets: ['latin'] })
 
-function Header(props: HeaderProps) {
+const headerLink = [
+  {
+    destination: '/quem-somos',
+    name: 'Quem somos',
+  },
+  {
+    destination: '/solucoes',
+    name: 'Soluções',
+  },
+  {
+    destination: '/preços',
+    name: 'Preços',
+  },
+  {
+    destination: '/contato',
+    name: 'Contato',
+  },
+  {
+    destination: '/login',
+    name: 'Login',
+  },
+]
+
+function Header() {
   return (
     <>
       <Head>
-        <title>Meddash - {props.title ?? 'Gerenciamento médico simplificado'}</title>
+        <title>Meddash - Gerenciamento médico simplificado</title>
       </Head>
       <Box>
-        This is the header
+        <Grid
+          maxWidth={{ xl: '1440px' }}
+          margin='0 auto'
+          height={{ xl: '88px' }}
+          alignItems={{ xl: 'center' }}
+          gridTemplateColumns={{ xl: '150px 10fr 1fr' }}
+          gap={{ xl: '56px' }}
+        >
+          <Image src='/images/logo.png' width={150} height={50} alt='Logo MedDash' />
+          <Flex
+            as={'nav'}
+            gap={{ xl: '32px' }}
+            justifyContent={{ xl: 'flex-end' }}
+            fontFamily={poppins.style.fontFamily}
+            fontWeight={poppins.style.fontWeight}
+            color={'gray.600'}
+          >
+            {headerLink.map((link, i) => {
+              return (
+                <Link href={link.destination} key={i}>
+                  {link.name}
+                </Link>
+              )
+            })}
+          </Flex>
+          <Button
+            colorScheme={'purple'}
+            boxShadow={'xl'}
+          >
+            Criar conta
+          </Button>
+        </Grid>
       </Box>
     </>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,4 @@
-import { Inter } from '@next/font/google'
 import { Footer, Header } from '../components'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
